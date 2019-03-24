d2f9d2b6f4d6904a32e677ffe8020a64c4c2d443
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-03-24 22:24:42 +0700

    callback hell

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+// Asynchronous
 console.log('Before');
 getUser(1,  (user) => {
     console.log('User', user);
 
     getRepositories(user.gitHubUserName, (repos) => {
         console.log('Repos', repos);
+
+        getCommits(repos, (commits) => {
+            // Callback hell ???
+        });
     });
 });
+console.log('After');
+
+// Synchronous
+console.log('Before');
+const user = getUser(1);
+const repos = getRepositories(user.gitHubUserName);
+const commits = getCommits(repos[0]);
 console.log('After');
 
 function getUser(id, callback) {
@@ -20,4 +32,8 @@ function getRepositories(username, callback) {
         console.log('Calling GitHub API...');
         callback(['repo1', 'repo2', 'repo3']);
     }, 2000);
+}
+
+function getCommits(repo, callback) {
+
 }
