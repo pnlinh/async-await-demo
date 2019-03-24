766092ff01d766bbde58eeb17fb392f6026f5a0f
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-03-24 21:36:32 +0700

    callback

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 console.log('Before');
-setTimeout(() => {
-    console.log('Reading a user from a database...');
-}, 2000);
+getUser(1,  (user) => {
+    console.log('User', user);
+
+    getRepositories(user.gitHubUserName, (repos) => {
+        console.log('Repos', repos);
+    });
+});
 console.log('After');
+
+function getUser(id, callback) {
+    setTimeout(() => {
+        console.log('Reading a user from a database...');
+        callback({id: id, gitHubUserName: 'pnlinh'});
+    }, 2000);
+}
+
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        console.log('Calling GitHub API...');
+        callback(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+}
