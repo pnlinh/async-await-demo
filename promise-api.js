cc9e4191571cdd3d1bb435a53e752a927ce3d346
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-03-25 08:01:26 +0700

    race promise

--- promise-api.js
+++ promise-api.js
@@ -1,5 +1,24 @@
-const p = Promise.resolve({id: 1});
-p.then(result => console.log(result));
+// const p = Promise.resolve({id: 1});
+// p.then(result => console.log(result));
+//
+// const p2 = Promise.reject('reason for rejection...');
+// p2.catch(error => console.log(error));
 
-const p2 = Promise.reject('reason for rejection...');
-p2.catch(error => console.log(error));
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('Async operation 1...');
+        resolve(1);
+        // reject(new Error('because something failed.'));
+    }, 2000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('Async operation 2...');
+        resolve(2);
+    }, 2000);
+});
+
+Promise.race([p1, p2])
+    .then(result => console.log(result))
+    .catch(error => console.log('Error', error.message));
